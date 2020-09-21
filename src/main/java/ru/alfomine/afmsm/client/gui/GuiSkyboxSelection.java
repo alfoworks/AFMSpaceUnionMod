63da242a7ac26c1e8c49fd65154648cfb02f54d3
Iterator <rob.bog@list.ru>
2020-09-21 14:29:47 +0300

    [ANY] Bump

--- src/main/java/ru/alfomine/afmsm/client/gui/GuiSkyboxSelection.java
+++ src/main/java/ru/alfomine/afmsm/client/gui/GuiSkyboxSelection.java
@@ -1,5 +1,6 @@
 package ru.alfomine.afmsm.client.gui;
 
+import cr0s.warpdrive.client.SkyBoxManager;
 import cr0s.warpdrive.render.RenderSpaceSky;
 import cr0s.warpdrive.render.skybox.ISkyBoxRenderer;
 import net.minecraft.client.Minecraft;
@@ -21,6 +22,7 @@ public class GuiSkyboxSelection extends CustomGui {
     int selected = -1;
     private int lWidth = 262;
     private int lHeight = 512;
+    List<String> warnList;
 
     public GuiSkyboxSelection(List<ISkyBoxRenderer> skyboxes) {
         this.skyboxes = skyboxes;
@@ -33,12 +35,15 @@ public class GuiSkyboxSelection extends CustomGui {
         GuiButton butt = new CustomButton(0, res.getScaledWidth() - lWidth + 7, 187, 50, 20, I18n.format("afmsm.text.gui_planetselection.button_select"));
         butt.enabled = false;
 
+        this.selected = skyboxes.indexOf(RenderSpaceSky.getInstance().currentRenderer);
+
         buttonList.add(butt);
 
         int listY = 37;
         int listHeight = Math.min(lHeight, res.getScaledHeight() - listY);
 
         list = new GuiSlotSkyboxSelection(skyboxes, this, res.getScaledWidth() - 200, listY, 200, listHeight, 86, width, height);
+        this.warnList = fontRenderer.listFormattedStringToWidth(I18n.format("afmsm.text.gui_skyboxselection.warntext"), 180);
     }
 
     @Override
@@ -47,7 +52,6 @@ public class GuiSkyboxSelection extends CustomGui {
 
         int mouseX = Mouse.getEventX() * this.width / this.mc.displayWidth;
         int mouseY = this.height - Mouse.getEventY() * this.height / this.mc.displayHeight - 1;
-
 
         list.handleMouseInput(mouseX, mouseY);
     }
@@ -61,8 +65,16 @@ public class GuiSkyboxSelection extends CustomGui {
         mc.renderEngine.bindTexture(mainGuiLoc);
         drawTexturedModalRect512(lX, 0, lWidth, lHeight, 0, 0, lWidth, lHeight);
 
+        drawTexturedModalRect512(0, 0, 189, 51, 262, 274, 189, 51);
+
         int titleX = lX + 15;
         drawString(fontRenderer, I18n.format("afmsm.text.gui_skyboxselection.title"), titleX, 5, 0xFFFFFF);
+
+        for (int i = 0; i < warnList.size(); i++) {
+            String toDraw = warnList.get(i);
+
+            drawString(fontRenderer, toDraw, 5, i * fontRenderer.FONT_HEIGHT + 5, 0xFFFFFF);
+        }
 
         list.drawScreen(mouseX, mouseY, partialTicks);
 
@@ -74,6 +86,17 @@ public class GuiSkyboxSelection extends CustomGui {
 
         buttonList.get(0).enabled = slotIdx != -1;
 
-        RenderSpaceSky.getInstance().setRenderer(slotIdx);
+        SkyBoxManager.setSkyBox(skyboxes.get(selected).getID());
+    }
+
+    @Override
+    protected void actionPerformed(GuiButton button) throws IOException {
+        super.actionPerformed(button);
+
+        if (button.id == 0) {
+            SkyBoxManager.saveSelectedSkyBox(skyboxes.get(selected).getID());
+
+            Minecraft.getMinecraft().displayGuiScreen(null);
+        }
     }
 }
