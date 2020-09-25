3b2239677f4d58919e8e8ca60c366f698879baa4
Iterator <rob.bog@list.ru>
2020-09-25 17:20:20 +0300

    [FIX] Различные фиксы

--- src/main/java/ru/alfomine/afmsm/AFMSpaceUnionMod.java
+++ src/main/java/ru/alfomine/afmsm/AFMSpaceUnionMod.java
@@ -1,12 +1,5 @@
 package ru.alfomine.afmsm;
 
-import cr0s.warpdrive.client.SkyBoxManager;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.GuiButton;
-import net.minecraft.client.gui.GuiOptions;
-import net.minecraftforge.client.event.GuiScreenEvent;
-import net.minecraftforge.client.event.RenderGameOverlayEvent;
-import net.minecraftforge.client.event.RenderGameOverlayEvent.ElementType;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.SidedProxy;
@@ -14,21 +7,12 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.common.gameevent.InputEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.lwjgl.input.Keyboard;
-import ru.alfomine.afmsm.client.gui.GuiPlanetSelection;
-import ru.alfomine.afmsm.client.gui.GuiSkyboxSelection;
-import ru.alfomine.afmsm.client.keybinds.KeyBinder;
-import ru.alfomine.afmsm.client.keybinds.KeyBinder.KeyBind;
 import ru.alfomine.afmsm.command.CommandPlanetSelectionGui;
 import ru.alfomine.afmsm.network.AFMSMPacketHandler;
 import ru.alfomine.afmsm.proxy.IProxy;
 import ru.alfomine.afmsm.server.PlanetConfig;
-
-import java.util.ArrayList;
 
 
 @Mod(
@@ -53,10 +37,11 @@ public class AFMSpaceUnionMod {
 	public void preInit(FMLPreInitializationEvent event) {
 		MinecraftForge.EVENT_BUS.register(new ModItems());
 		MinecraftForge.EVENT_BUS.register(this);
-		
+		MinecraftForge.EVENT_BUS.register(proxy);
+
 		ModItems.init();
 		AFMSMPacketHandler.init();
-		
+
 		proxy.preInit(event);
 	}
 	
@@ -79,42 +64,5 @@ public class AFMSpaceUnionMod {
 	@Mod.EventHandler
 	public void serverStarting(FMLServerStartingEvent event) {
 		event.registerServerCommand(new CommandPlanetSelectionGui());
-	}
-	
-	@SubscribeEvent
-	public void onKeyInput(InputEvent.KeyInputEvent event) {
-		for (KeyBind keyBind : KeyBinder.keyBinds) {
-			if (Keyboard.isKeyDown(keyBind.getBinding().getKeyCode())) {
-				keyBind.onPress(event);
-			}
-		}
-	}
-	
-	@SubscribeEvent
-	public void onRenderOverlay(RenderGameOverlayEvent.Chat event) {
-		if (GuiPlanetSelection.active) {
-			event.setCanceled(true);
-		}
-	}
-
-	@SubscribeEvent
-	public void onRenderOverlay(RenderGameOverlayEvent event) {
-		if (GuiPlanetSelection.active && (event.getType() == ElementType.HOTBAR || event.getType() == ElementType.CROSSHAIRS)) {
-			event.setCanceled(true);
-		}
-	}
-
-	@SubscribeEvent
-	public void onScreenInit(GuiScreenEvent.InitGuiEvent.Post event) {
-        if (!(event.getGui() instanceof GuiOptions)) return;
-
-		event.getButtonList().add(new GuiButton(1337, 0, event.getGui().height - 20, 100, 20, "WarpDrive Skybox"));
-    }
-
-	@SubscribeEvent
-	public void onGuiScreenAction(GuiScreenEvent.ActionPerformedEvent.Post event) {
-		if (event.getButton().id != 1337) return;
-
-		Minecraft.getMinecraft().displayGuiScreen(new GuiSkyboxSelection(new ArrayList<>(SkyBoxManager.skyboxes.values())));
 	}
 }
--- src/main/java/ru/alfomine/afmsm/item/ItemSolarAtlas.java
+++ src/main/java/ru/alfomine/afmsm/item/ItemSolarAtlas.java
@@ -1,31 +1,25 @@
 package ru.alfomine.afmsm.item;
 
-import net.minecraft.client.Minecraft;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
-import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumHand;
-import net.minecraft.util.text.TextComponentString;
 import net.minecraft.world.World;
 import ru.alfomine.afmsm.network.AFMSMPacketHandler;
-import ru.alfomine.afmsm.network.message.MessageAdminItem;
 import ru.alfomine.afmsm.network.message.MessagePlanetaryGui;
 import ru.alfomine.afmsm.planet.SpaceData;
-
-import java.util.ArrayList;
 
 public class ItemSolarAtlas extends Item {
 	
 	public ItemSolarAtlas(String unlocalized) {
-		setTranslationKey(unlocalized);
-		setRegistryName(unlocalized);
-		setCreativeTab(CreativeTabs.MISC);
-		setMaxStackSize(1);
+		this.setTranslationKey(unlocalized);
+		this.setRegistryName(unlocalized);
+		this.setCreativeTab(CreativeTabs.MISC);
+		this.setMaxStackSize(1);
 	}
 	
 	@SuppressWarnings("NullableProblems")
