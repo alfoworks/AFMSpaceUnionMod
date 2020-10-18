96abf89144d93c2c86b20adad823d0f590002af9
Iterator <rob.bog@list.ru>
2020-10-18 23:29:18 +0300

    [ANY] Добавление депенденси

--- src/main/java/ru/alfomine/afmsm/world/Lightmap.java
+++ src/main/java/ru/alfomine/afmsm/world/Lightmap.java
@@ -1,20 +1,2 @@
-package ru.alfomine.afmsm.world;
-
-public class Lightmap {
-    public static float hue;
-
-    public static void getLightMap(float[] lightmap) {
-        for (int i = 0; i <= 15; ++i) {
-            lightmap[i] = 0xff000000 | 0xFF0000;
-        }
-        for (int i = 0; i <= 15; ++i) {
-            lightmap[i] = 0xff000000 | 0xFF0000;
-        }
-
-        hue += 0.1;
-
-        if (hue >= 1) {
-            hue = 0;
-        }
-    }
+package ru.alfomine.afmsm.world;public class Lightmap {
 }
